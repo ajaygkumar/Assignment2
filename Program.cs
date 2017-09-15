6bd6b986e1194cda117264326928835978642d3c
ajaygkumar <31571077+ajaygkumar@users.noreply.github.com>
2017-09-15 00:37:59 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace passbyvalue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c=1;
+            sample(c);
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+        public static void sample(int b)
+        {
+            b = 100;
+        }
+    }
+}
